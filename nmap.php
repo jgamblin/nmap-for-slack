656a79bd9536800556639a0a761a28f6faddc997
reedphish <reedphish@outlook.com>
2016-05-14 11:43:38 -0700

    Sanitizing input

--- nmap.php
+++ nmap.php
@@ -1,6 +1,7 @@
 <?php
 
-/**
+/***********************************************************************************************
+ *
  * REQUIREMENTS
  * A custom slash command on a Slack team
  * A web server running PHP5 with cURL enabled and NMap 7.
@@ -15,13 +16,21 @@
  * Decide whether you want this command to show in the 
  * autocomplete list for slash commands.
  * If you do, enter a short description and usage hint.
- */
+ *
+ * Co-author: reedphish 
+ * 
+ * Reedphish contact information: 
+ * reedphish@outlook.com>, github.com/reedphish, reedphish.wordpress.com, twitter.com/reedphish
+ ***********************************************************************************************/
 
-// Master token, replace this with the token from your slash command configuration page
+/**
+ * Master token, replace this with the token from your slash command configuration page
+ */
 $mastertoken = "motorhead";
 
-print_r($_POST);
-
+/**
+ * Processing POST request
+ */
 if(isset($_POST["command"]) && isset($_POST["token"]) && isset($_POST['text'])) {
 	// Grab some of the values from the slash command, create vars for post back to Slack
 	$command = $_POST['command'];
@@ -35,18 +44,26 @@ if(isset($_POST["command"]) && isset($_POST["token"]) && isset($_POST['text'])) 
 
 	$user_agent = "Slack/1.0";
 
-	$cmd = "nmap --top-ports 50 --open {$text}";
-	print($cmd);
+	if(isvalid($text)) {
+		$cmd = "nmap --top-ports 50 --open {$text}";
 
-	/*
-	$proc = popen($cmd, 'r');
-	
-	while (!feof($proc))
-	{
-	    echo fread($proc, 4096);
-	    @ flush();
+		$proc = popen($cmd, 'r');
+		
+		while (!feof($proc))
+		{
+		    echo fread($proc, 4096);
+		    @ flush();
+		}
+	} else {
+		die("Address is not legal");
 	}
-	*/
 } else {
 	die("Failure processing request. Please check required parameters and transport verb.");
+}
+
+/**
+ * Check if string contains illegal characters
+ */
+function isValid(string $content) {
+	return preg_match('/^[a-zA-Z0-9\.]*$/', $content);
 }
