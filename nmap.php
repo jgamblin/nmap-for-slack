589d712184ac7887591a5281aee14a540d6efb57
reedphish <reedphish@outlook.com>
2016-05-14 11:07:16 -0700

    Restructuring nmap.php before santizing input

--- nmap.php
+++ nmap.php
@@ -1,39 +1,52 @@
 <?php
-/*
-REQUIREMENTS
-* A custom slash command on a Slack team
-* A web server running PHP5 with cURL enabled and NMap 7.
-USAGE
-* Place this script on a server running PHP5 with cURL.
-* Set up a new custom slash command on your Slack team: 
-  http://*.slack.com/services/new/slash-commands
-* Under "Choose a command", enter whatever you want for 
-  the command. /dns is easy to remember.
-* Under "URL", enter the URL for the script on your server.
-* Leave "Method" set to "Post".
-* Decide whether you want this command to show in the 
-  autocomplete list for slash commands.
-* If you do, enter a short description and usage hint.
-*/
-# Grab some of the values from the slash command, create vars for post back to Slack
-$command = $_POST['command'];
-$text = $_POST['text'];
-$token = $_POST['token'];
-# Check the token and make sure the request is from our team 
-if($token != '9OPGYcRv8sHXBJwWpfr3JKrM'){ #replace this with the token from your slash command configuration page
-  $msg = "The token for the slash command doesn't match. Check your script.";
-  die($msg);
-  echo $msg;
-}
 
-$user_agent = "Slack/1.0";
+/**
+ * REQUIREMENTS
+ * A custom slash command on a Slack team
+ * A web server running PHP5 with cURL enabled and NMap 7.
+ * USAGE
+ * Place this script on a server running PHP5 with cURL.
+ * Set up a new custom slash command on your Slack team: 
+ * http://*.slack.com/services/new/slash-commands
+ * Under "Choose a command", enter whatever you want for 
+ * the command. /dns is easy to remember.
+ * Under "URL", enter the URL for the script on your server.
+ * Leave "Method" set to "Post".
+ * Decide whether you want this command to show in the 
+ * autocomplete list for slash commands.
+ * If you do, enter a short description and usage hint.
+ */
 
-$cmd = "nmap --top-ports 50 --open ".$text;
-#while (@ ob_end_flush()); // end all output buffers if any
+// Master token, replace this with the token from your slash command configuration page
+$mastertoken = "motorhead";
 
-$proc = popen($cmd, 'r');
-while (!feof($proc))
-{
-    echo fread($proc, 4096);
-    @ flush();
+print_r($_POST);
+
+if(isset($_POST["command"]) && isset($_POST["token"]) && isset($_POST['text'])) {
+	// Grab some of the values from the slash command, create vars for post back to Slack
+	$command = $_POST['command'];
+	$token = $_POST['token'];
+	$text = $_POST['text'];
+
+	// Check the token and make sure the request is from our team 
+	if($token != $mastertoken) {
+		die("The token for the slash command doesn't match. Check your script.");
+	}
+
+	$user_agent = "Slack/1.0";
+
+	$cmd = "nmap --top-ports 50 --open {$text}";
+	print($cmd);
+
+	/*
+	$proc = popen($cmd, 'r');
+	
+	while (!feof($proc))
+	{
+	    echo fread($proc, 4096);
+	    @ flush();
+	}
+	*/
+} else {
+	die("Failure processing request. Please check required parameters and transport verb.");
 }
